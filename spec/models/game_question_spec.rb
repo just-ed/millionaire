0a3d1e661506eae8e43102a5e06d16e6c7eb9b39
just-ed <ed_tomeyan@mail.ru>
2019-05-08 22:55:20 +0300

    game_question friend_call test

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -76,5 +76,15 @@ RSpec.describe GameQuestion, type: :model do
       expect(ff).to include('b')
       expect(ff.size).to eq 2
     end
+
+    it 'correct friend_call' do
+      expect(game_question.help_hash).not_to include(:friend_call)
+
+      game_question.add_friend_call
+
+      expect(game_question.help_hash).to include(:friend_call)
+
+      expect(%w[A B C D]).to include(game_question.help_hash[:friend_call].last)
+    end
   end
 end
